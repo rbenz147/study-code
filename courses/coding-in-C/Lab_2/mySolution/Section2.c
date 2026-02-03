789723c519d06434891f6775f3849e559f1732b0
Roman Benz <kontakt@romanbenz.com>
2026-02-03 18:53:58 +0100

    Added final solution by adding 2 loops

---
+++ courses/coding-in-C/Lab_2/mySolution/Section2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+char array[9] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};  
+
+int main(void)
+{
+    for (int i = 8; i >= 0; i--)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%c%d ", array[j], i);
+        }
+        printf("\n");
+        
+    }
+    
+    
+}
